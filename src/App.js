6ba72dc9b10669f757dc6357319fb5990968e6d4
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-12-16 15:31:17 -0800

    made app into a class to keep track of state

--- src/App.js
+++ src/App.js
@@ -1,47 +1,63 @@
-import React from "react";
+
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import System from './components/System.js';
 import About from './components/About.js';
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+class App extends Component {
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+     
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+        <p>This will always be here</p>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    )};
+
 }
+
+export default App
 
 function Home() {
   return <h2>Home</h2>;
---
+++ src/components/System.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class System extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+     
+    }
+  }
+
+  render(){
+  return (
+    <h2>System</h2>
+  )
+  
+}
+  
+}
+
+export default System;
